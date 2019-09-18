a3caa367568608dc8810cac9489d48c1649da11a
Peter Young <young40@gmail.com>
2019-09-18 23:57:46 +0800

    Initial Commit

---
+++ SwiftUILearning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUILearning
+//
+//  Created by 杨世玲 on 2019/9/18.
+//  Copyright © 2019 杨世玲. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
